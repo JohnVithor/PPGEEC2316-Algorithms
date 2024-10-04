bc8912e3ad456dc195f020e2aded2058f106905c
JohnVithor <jv.venceslau.c@gmail.com>
2024-10-04 17:57:41 -0300

    WIP: update strassen

--- c/src/matrix/strassen.c
+++ c/src/matrix/strassen.c
@@ -2,6 +2,13 @@
 #include "matrix.h"
 
 void matrix_multiply_strassen(int* a, int* b, int* c, unsigned int n) {
+    // https://www.cs.cmu.edu/afs/cs/academic/class/15750-s17/ScribeNotes/lecture1.pdf
+    int* buffer = malloc(4*n*n*sizeof(int));
+    matrix_multiply_strassen_internal(a, b, c, n, buffer);
+    free(buffer);
+}
+
+void matrix_multiply_strassen_internal(int* a, int* b, int* c, unsigned int n, int* buffer) {
     if (n == 1) {
         c[0] = a[0] * b[0];
         return;
@@ -9,29 +16,20 @@ void matrix_multiply_strassen(int* a, int* b, int* c, unsigned int n) {
 
     unsigned int mid = n / 2;
 
-    int* a11 = malloc(mid*mid*sizeof(int));
-    int* a12 = malloc(mid*mid*sizeof(int));
-    int* a21 = malloc(mid*mid*sizeof(int));
-    int* a22 = malloc(mid*mid*sizeof(int));
-    int* b11 = malloc(mid*mid*sizeof(int));
-    int* b12 = malloc(mid*mid*sizeof(int));
-    int* b21 = malloc(mid*mid*sizeof(int));
-    int* b22 = malloc(mid*mid*sizeof(int));
-    int* c11 = malloc(mid*mid*sizeof(int));
-    int* c12 = malloc(mid*mid*sizeof(int));
-    int* c21 = malloc(mid*mid*sizeof(int));
-    int* c22 = malloc(mid*mid*sizeof(int));
+    int* a11 = buffer;
+    int* a12 = a11 + (mid*mid);
+    int* a21 = a12 + (mid*mid);
+    int* a22 = a21 + (mid*mid);
+    int* b11 = a22 + (mid*mid);
+    int* b12 = b11 + (mid*mid);
+    int* b21 = b12 + (mid*mid);
+    int* b22 = b21 + (mid*mid);
 
-    int* m1 = malloc(mid*mid*sizeof(int));
-    int* m2 = malloc(mid*mid*sizeof(int));
-    int* m3 = malloc(mid*mid*sizeof(int));
-    int* m4 = malloc(mid*mid*sizeof(int));
-    int* m5 = malloc(mid*mid*sizeof(int));
-    int* m6 = malloc(mid*mid*sizeof(int));
-    int* m7 = malloc(mid*mid*sizeof(int));
+    int* m = b22 + (mid*mid);
 
-    int* aux1 = malloc(mid*mid*sizeof(int));
-    int* aux2 = malloc(mid*mid*sizeof(int));
+    int* aux1 = m + (mid*mid);
+    int* aux2 = aux1 + (mid*mid);
+    int* remaining_buffer = aux2 + (mid*mid);
 
     for (unsigned int i = 0; i < mid; ++i) {
         for (unsigned int j = 0; j < mid; ++j) {
@@ -43,72 +41,81 @@ void matrix_multiply_strassen(int* a, int* b, int* c, unsigned int n) {
             b12[i * mid + j] = b[i         * n + j + mid];
             b21[i * mid + j] = b[(i + mid) * n + j      ];
             b22[i * mid + j] = b[(i + mid) * n + j + mid];
+            c[i         * n + j       ] = 0;
+            c[i         * n + j + mid ] = 0;
+            c[(i + mid) * n + j       ] = 0;
+            c[(i + mid) * n + j + mid ] = 0;
         }
     }
 
     matrix_add(a11, a22, aux1, mid);
     matrix_add(b11, b22, aux2, mid);
-    matrix_multiply_strassen(aux1, aux2, m1, mid);
+    matrix_multiply_strassen_internal(aux1, aux2, m, mid, remaining_buffer);
     
+    for (unsigned int i = 0; i < mid; ++i) {
+        for (unsigned int j = 0; j < mid; ++j) {
+            c[i         * n + j       ] += m[i * mid + j];
+            c[(i + mid) * n + j + mid ] += m[i * mid + j];
+        }
+    }
+
     matrix_add(a21, a22, aux1, mid);
-    matrix_multiply_strassen(aux1, b11, m2, mid);
+    matrix_multiply_strassen_internal(aux1, b11, m, mid, remaining_buffer);
+
+    for (unsigned int i = 0; i < mid; ++i) {
+        for (unsigned int j = 0; j < mid; ++j) {
+            c[(i + mid) * n + j       ] += m[i * mid + j];
+            c[(i + mid) * n + j + mid ] -= m[i * mid + j];
+        }
+    }
 
     matrix_subtract(b12, b22, aux2, mid);
-    matrix_multiply_strassen(a11, aux2, m3, mid);
+    matrix_multiply_strassen_internal(a11, aux2, m, mid, remaining_buffer);
+
+    for (unsigned int i = 0; i < mid; ++i) {
+        for (unsigned int j = 0; j < mid; ++j) {
+            c[i         * n + j + mid ] += m[i * mid + j];
+            c[(i + mid) * n + j + mid ] += m[i * mid + j];
+        }
+    }
 
     matrix_subtract(b21, b11, aux1, mid);
-    matrix_multiply_strassen(a22, aux1, m4, mid);
+    matrix_multiply_strassen_internal(a22, aux1, m, mid, remaining_buffer);
+
+    for (unsigned int i = 0; i < mid; ++i) {
+        for (unsigned int j = 0; j < mid; ++j) {
+            c[i         * n + j] += m[i * mid + j];
+            c[(i + mid) * n + j] += m[i * mid + j];
+        }
+    }
 
     matrix_add(a11, a12, aux2, mid);
-    matrix_multiply_strassen(aux2, b22, m5, mid);
+    matrix_multiply_strassen_internal(aux2, b22, m, mid, remaining_buffer);
+
+    for (unsigned int i = 0; i < mid; ++i) {
+        for (unsigned int j = 0; j < mid; ++j) {
+            c[i * n + j       ] -= m[i * mid + j];
+            c[i * n + j + mid ] += m[i * mid + j];
+        }
+    }
 
     matrix_subtract(a21, a11, aux1, mid);
     matrix_add(b11, b12, aux2, mid);
-    matrix_multiply_strassen(aux1, aux2, m6, mid);
+    matrix_multiply_strassen_internal(aux1, aux2, m, mid, remaining_buffer);
+
+    for (unsigned int i = 0; i < mid; ++i) {
+        for (unsigned int j = 0; j < mid; ++j) {
+            c[(i + mid) * n + j + mid ] += m[i * mid + j];
+        }
+    }
 
     matrix_subtract(a12, a22, aux1, mid);
     matrix_add(b21, b22, aux2, mid);
-    matrix_multiply_strassen(aux1, aux2, m7, mid);
+    matrix_multiply_strassen_internal(aux1, aux2, m, mid, remaining_buffer);
 
     for (unsigned int i = 0; i < mid; ++i) {
         for (unsigned int j = 0; j < mid; ++j) {
-            c11[i * mid + j] = m1[i * mid + j] + m4[i * mid + j] - m5[i * mid + j] + m7[i * mid + j];
-            c12[i * mid + j] = m3[i * mid + j] + m5[i * mid + j];
-            c21[i * mid + j] = m2[i * mid + j] + m4[i * mid + j];
-            c22[i * mid + j] = m1[i * mid + j] - m2[i * mid + j] + m3[i * mid + j] + m6[i * mid + j];
+            c[i * n + j] += m[i * mid + j];
         }
     }
-
-    for (unsigned int i = 0; i < mid; ++i) {
-        for (unsigned int j = 0; j < mid; ++j) {
-            c[i         * n + j       ] = c11[i * mid + j];
-            c[i         * n + j + mid ] = c12[i * mid + j];
-            c[(i + mid) * n + j       ] = c21[i * mid + j];
-            c[(i + mid) * n + j + mid ] = c22[i * mid + j];
-        }
-    }
-
-    free(a11);
-    free(a12);
-    free(a21);
-    free(a22);
-    free(b11);
-    free(b12);
-    free(b21);
-    free(b22);
-    free(c11);
-    free(c12);
-    free(c21);
-    free(c22);
-
-    free(m1);
-    free(m2);
-    free(m3);
-    free(m4);
-    free(m5);
-    free(m6);
-    free(m7);
-
-    free(aux1);
-    free(aux2);
 }
--- c/src/runners/matrix.c
+++ c/src/runners/matrix.c
@@ -5,45 +5,54 @@
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Uso: %s <n> <seed> <algoritmo> (n = 2^x, para algum x >= 1 e seed >=0])\n", argv[0]);
-        printf("algoritmo:\n0 - classic multiply\n1 - strassen multiply\n");
+    if (argc != 3) {
+        printf("Uso: %s <n> <seed> (n = 2^x, para algum x >= 1 e seed >=0])\n", argv[0]);
         return 1;
     }
     int n = atoi(argv[1]);
     int seed = atoi(argv[2]);
-    int algoritmo = atoi(argv[3]);
 
     if (n < 2 || (n & (n - 1)) != 0 || seed < 0) {
-        printf("Uso: %s <n> <seed> <algoritmo> (n = 2^x, para algum x >= 1 e seed >=0])\n", argv[0]);
-        printf("algoritmo:\n0 - classic multiply\n1 - strassen multiply\n");
+        printf("Uso: %s <n> <seed> (n = 2^x, para algum x >= 1 e seed >=0])\n", argv[0]);
         return 1;
     }
 
-    void (*ops[2])(int* a, int* b, int* c, unsigned int n) = {matrix_multiply, matrix_multiply_strassen};
-    char* op_names[2] = {"classic", "strassen"};
-
-    srand(seed);
     int* a = (int*) malloc(n*n * sizeof(int)); 
     int* b = (int*) malloc(n*n * sizeof(int)); 
     int* c = (int*) malloc(n*n * sizeof(int)); 
+    int* d = (int*) malloc(n*n * sizeof(int)); 
+    srand(seed);
     for (int i = 0; i < n*n; i++){
         a[i] = rand();
         b[i] = rand();
         c[i] = 0;
+        d[i] = 0;
     }
 
     struct timespec ts_start;
     struct timespec ts_end;
     clock_gettime(CLOCK_MONOTONIC, &ts_start);
-    (ops[algoritmo])(a, b, c, n);
+    matrix_multiply(a, b, c, n);
     clock_gettime(CLOCK_MONOTONIC, &ts_end);
-    double time_spent = (double)(ts_end.tv_sec - ts_start.tv_sec) + ((double)(ts_end.tv_nsec - ts_start.tv_nsec)/1000000000L);
+    double time_spent_classic = (double)(ts_end.tv_sec - ts_start.tv_sec) + ((double)(ts_end.tv_nsec - ts_start.tv_nsec)/1000000000L);
 
-    printf("%s,%lf\n", op_names[algoritmo], time_spent);
+    clock_gettime(CLOCK_MONOTONIC, &ts_start);
+    matrix_multiply_strassen(a, b, d, n);
+    clock_gettime(CLOCK_MONOTONIC, &ts_end);
+    double time_spent_strassen = (double)(ts_end.tv_sec - ts_start.tv_sec) + ((double)(ts_end.tv_nsec - ts_start.tv_nsec)/1000000000L);
+
+    for(int i = 0; i < n*n; i++) {
+        if (c[i] != d[i]) {
+            printf("Erro: c[%d] = %d != %d = d[%d]\n", i, c[i], d[i], i);
+            return 1;
+        }
+    }
+
+    printf("%lf,%lf\n", time_spent_classic, time_spent_strassen);
 
     free(a);
     free(b);
     free(c);
+    free(d);
     return 0;
 }
