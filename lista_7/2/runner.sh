08cb8a645e26aa17722a57eed012803a013e47d2
JohnVithor <jv.venceslau.c@gmail.com>
2024-11-27 15:54:44 -0300

    add runner list 7-2

---
+++ lista_7/2/runner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=matrix_multiply_PPGEEC
+#SBATCH --output=results.csv
+#SBATCH --error=errors.txt
+#SBATCH --hint=compute_bound
+#SBATCH --partition=amd-512
+#SBATCH --time=1-00:00:00
+#SBATCH --qos=qos1
+
+# Create or clear the results.csv file
+echo "size,i,classic,classic_parallel,strassen,strassen_parallel,transposed,transposed_parallel" > results.csv
+
+# Set the problems sizes
+for size in 4 8 16 32 64 128 256 512 1024 2048 4096; do
+  for i in {1..10}; do
+    # Capture the output of the binary
+    output=$(taskset -c 0 "../../c/bin/matrix_multiply" $size 0)
+    # Write the binary name, run number, and output to the CSV file
+    echo "$size,$i,$output" >> results.csv
+  done
+done
