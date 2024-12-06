cffec124f914ab00d0a60bf764419523ef86ede6
JohnVithor <jv.venceslau.c@gmail.com>
2024-12-06 16:29:33 -0300

    add more linear algebra

--- rust/src/algorithms/linear_algebra.rs
+++ rust/src/algorithms/linear_algebra.rs
@@ -50,6 +50,41 @@ where
     backward_substitution(U, &forward_substitution(L, &(P * b)))
 }
 
+pub fn lu_decomposition<const S: usize>(mut matrix: Matrix<S, S>) -> (Matrix<S, S>, Matrix<S, S>)
+where
+    [(); S * S]:,
+{
+    let mut L = Matrix::new(0.0);
+    let mut U = Matrix::new(0.0);
+    for k in 0..S {
+        L[(k, k)] = 1.0;
+        U[(k, k)] = matrix[(k, k)];
+        for i in k..S {
+            L[(i, k)] = matrix[(i, k)] / matrix[(k, k)];
+            U[(k, i)] = matrix[(k, i)];
+        }
+        for i in k..S {
+            for j in k..S {
+                matrix[(i, j)] -= L[(i, k)] * U[(k, j)];
+            }
+        }
+    }
+    (L, U)
+}
+
+pub fn lup_decomposition<const S: usize>(
+    mut matrix: Matrix<S, S>,
+) -> (Matrix<S, S>, Matrix<S, S>, Matrix<S, S>)
+where
+    [(); S * S]:,
+{
+    let mut p = [0; S];
+    for i in 0..S {
+        p[i] = i;
+    }
+    todo!("Implement LUP decomposition")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -60,13 +95,8 @@ mod tests {
         //     data: [1.0, 2.0, 0.0, 3.0, 4.0, 4.0, 5.0, 6.0, 3.0],
         // };
         let b = Vector::from([3.0, 7.0, 8.0]);
-        let L: Matrix<3, 3> = Matrix {
-            data: [1.0, 0.0, 0.0, 0.2, 1.0, 0.0, 0.6, 0.5, 1.0],
-        };
-
-        let P = Matrix {
-            data: [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-        };
+        let L = Matrix::from([[1.0, 0.0, 0.0], [0.2, 1.0, 0.0], [0.6, 0.5, 1.0]]);
+        let P = Matrix::from([[0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]]);
         let Pb = P * b;
         let result = forward_substitution(&L, &Pb);
         assert_eq!(result, Vector::from([8.0, 1.4, 1.5]));
@@ -74,11 +104,48 @@ mod tests {
 
     #[test]
     fn test_backward_substitution() {
-        let U = Matrix {
-            data: [5.0, 6.0, 3.0, 0.0, 0.8, -0.6, 0.0, 0.0, 2.5],
-        };
+        let U = Matrix::from([[5.0, 6.0, 3.0], [0.0, 0.8, -0.6], [0.0, 0.0, 2.5]]);
         let y = Vector::from([8.0, 1.4, 1.5]);
         let result = backward_substitution(&U, &y);
         assert_eq!(result, Vector::from([-1.4, 2.2, 0.6]));
+    }
+
+    #[test]
+    fn test_lup_solve() {
+        let L = Matrix::from([[1.0, 0.0, 0.0], [0.2, 1.0, 0.0], [0.6, 0.5, 1.0]]);
+        let U = Matrix::from([[5.0, 6.0, 3.0], [0.0, 0.8, -0.6], [0.0, 0.0, 2.5]]);
+        let P = Matrix::from([[0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]]);
+        let b = Vector::from([3.0, 7.0, 8.0]);
+        let result = lup_solve(&L, &U, &P, &b);
+        assert_eq!(result, Vector::from([-1.4, 2.2, 0.6]));
+    }
+
+    #[test]
+    fn test_lu_decomposition() {
+        let A = Matrix::from([
+            [2.0, 3.0, 1.0, 5.0],
+            [6.0, 13.0, 5.0, 19.0],
+            [2.0, 19.0, 10.0, 23.0],
+            [4.0, 10.0, 11.0, 31.0],
+        ]);
+        let (L, U) = lu_decomposition(A);
+        assert_eq!(
+            L,
+            Matrix::from([
+                [1.0, 0.0, 0.0, 0.0],
+                [3.0, 1.0, 0.0, 0.0],
+                [1.0, 4.0, 1.0, 0.0],
+                [2.0, 1.0, 7.0, 1.0],
+            ])
+        );
+        assert_eq!(
+            U,
+            Matrix::from([
+                [2.0, 3.0, 1.0, 5.0],
+                [0.0, 4.0, 2.0, 4.0],
+                [0.0, 0.0, 1.0, 2.0],
+                [0.0, 0.0, 0.0, 3.0],
+            ])
+        );
     }
 }
