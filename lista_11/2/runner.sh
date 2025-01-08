dc37244184a1fc629233b1f998cea9b0e970175a
JohnVithor <jv.venceslau.c@gmail.com>
2025-01-08 16:19:38 -0300

    add runner script and visualization for DFT/FFT performance analysis

---
+++ lista_11/2/runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create or clear the results.csv file
+echo "size,i,dft_time,fft_time" > results_r.csv
+
+# Set the problems sizes
+for size in 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288; do
+  for i in {1..10}; do
+    # Capture the output of the binary
+    output=$(taskset -c 1 "../../rust/target/release/dft_fft" $size 0)
+    # Write the binary name, run number, and output to the CSV file
+    echo "$size,$i,$output" >> results_r.csv
+  done
+done
---
+++ lista_11/2/visualize.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("results.csv")
+df.groupby(["size"]).mean().reset_index().drop("run", axis=1).to_csv(
+    "results_grouped.csv", index=False, float_format="%.6f"
+)
+df_costs = df[["size", "run", "mtf_cost", "foresee_cost"]].copy()
+df_costs.loc[:, "Competitivity"] = df_costs["mtf_cost"] / df_costs["foresee_cost"]
+
+df_times = df[["size", "run", "mtf_time", "foresee_time"]].copy()
+df_times.loc[:, "Competitivity"] = df_times["mtf_time"] / df_times["foresee_time"]
+
+
+fig = sns.lineplot(data=df_costs, x="size", y="Competitivity", marker="o")
+plt.tight_layout()
+plt.savefig("results1.png")
+plt.clf()
+
+fig = sns.lineplot(data=df_costs, x="size", y="Competitivity", marker="o")
+fig.set(xscale="log")
+fig.set(yscale="log")
+
+plt.tight_layout()
+plt.savefig("results2.png")
+plt.clf()
+
+fig = sns.lineplot(data=df_times, x="size", y="Competitivity", marker="o")
+plt.tight_layout()
+plt.savefig("results3.png")
+plt.clf()
+
+fig = sns.lineplot(data=df_times, x="size", y="Competitivity", marker="o")
+fig.set(xscale="log")
+fig.set(yscale="log")
+
+plt.tight_layout()
+plt.savefig("results4.png")
